01c1909df5cace2bde80c43697739e247a1811df
sergeychuvakin <sergeychuvakin1@mail.ru>
2020-03-31 15:41:30 +0300

    hello world, from Lena

--- file.py
+++ file.py
@@ -1,9 +1,7 @@
 from sklearn import models
 import pandas as pd
 import numpy as np
-<<<<<<< HEAD
 import operatror as op
-=======
 from random import random
 df = pd.DataFrame({"a":[1,2,3]})
->>>>>>> hotfix2
+
